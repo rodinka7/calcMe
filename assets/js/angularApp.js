eae2daaf7c4de1edffa0b35378c5a59e6a70c1a2
TaniaDan <rodinka7@yahoo.com>
2017-07-25 15:53:55 +0300

    Developing

--- assets/js/angularApp.js
+++ assets/js/angularApp.js
@@ -30,5 +30,40 @@
 				return true;
 			}		
 		};
+
+		$scope.graph = function(){
+			var container = document.querySelector('svg'),
+				a = $scope.a || 1,
+				b = $scope.b || 1,
+				c = $scope.c || 0,
+				d,
+				x1,
+				x2,
+				x0,
+				y0,
+				line;
+
+			d = Math.pow(b, 2) - 4*a*c;
+			
+			if (d < 0) {
+				console.log('Уравнение не имеет корней!');
+			} else if (d === 0) {
+				x1 = Math.round(-b/(2*a));
+				x2 = x1;
+			} else {	
+				x1 = Math.round((-b - Math.sqrt($scope.d))/(2*a));
+				x2 = Math.round((-b + Math.sqrt($scope.d))/(2*a));
+			}	
+
+			/*Начало системы кординат (265,327)*/
+			x0 = Math.round(-b/(2*a));
+			y0 = Math.round(a*x0*x0 + b*x0 + c);
+
+			line = document.createElement('polyline');
+
+			line.innerHTML = `<polyline points="${265+x1},327 ${265+x0},${327+y0} ${265+x2},327" stroke="#594C91" stroke-width="2"></polyline>`;
+
+			container.appendChild(line);
+		}
 	})
 })();
